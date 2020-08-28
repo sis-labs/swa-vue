63963731638839efe6939efc3b17684642a878b1
lefebvrm <mlefebvre@groupemutuel.ch>
2020-08-28 07:47:25 +0200

    fix: resolve address search from raw request
    Signed-off-by: lefebvrm <mlefebvre@groupemutuel.ch>

--- src/components/LuceneSearch.vue
+++ src/components/LuceneSearch.vue
@@ -6,17 +6,28 @@
         <div class="md-title">Lucene search</div>
       </md-card-header>
       <md-card-content>
-        <md-autocomplete
-            class="search"
-            v-model="selectedEmployee"
-            :md-options="employees"
-            @md-changed="onInputChanged"
-            @md-selected="onValueSelected"
-            md-layout="box">
-          <label>Search...</label>
-        </md-autocomplete>
+        <md-field>
+          <label>Initial Value</label>
+          <md-input v-model="searchQuery" v-on:blur="onInputBlured"></md-input>
+        </md-field>
+
+        <md-table  v-model="addresses" md-card>
+          <md-table-toolbar>
+            <h1 class="md-title">Addresses</h1>
+          </md-table-toolbar>
+          <md-table-row slot="md-table-row" slot-scope="{ item }">
+            <md-table-cell md-label="Nb">{{ item.nb }}</md-table-cell>
+            <md-table-cell md-label="Street">{{ item.street }}</md-table-cell>
+            <md-table-cell md-label="Zip">{{ item.zip }}</md-table-cell>
+            <md-table-cell md-label="City">{{ item.city }}</md-table-cell>
+            <md-table-cell md-label="Commune">{{ item.commune }}</md-table-cell>
+            <md-table-cell md-label="State">{{ item.state }}</md-table-cell>
+            <md-table-cell md-label="Country">{{ item.country }}</md-table-cell>
+          </md-table-row>
+        </md-table>
+
         <md-card-actions>
-          <md-button type="submit" class="md-primary" :disabled="sending">Send</md-button>
+          <md-button type="submit" class="md-primary">Help</md-button>
         </md-card-actions>
       </md-card-content>
     </md-card>
@@ -24,34 +35,23 @@
 </template>
 
 <script>
+  import {search} from "@/lib/search-service";
+
   export default {
     name: "LuceneSearch",
     data: () => ({
-      selectedEmployee: null,
+      searchQuery: null,
       sending: false,
-      employees: [
-        'Jim Halpert',
-        'Dwight Schrute',
-        'Michael Scott',
-        'Pam Beesly',
-        'Angela Martin',
-        'Kelly Kapoor',
-        'Ryan Howard',
-        'Kevin Malone',
-        'Creed Bratton',
-        'Oscar Nunez',
-        'Toby Flenderson',
-        'Stanley Hudson',
-        'Meredith Palmer',
-        'Phyllis Lapin-Vance'
-      ]
+      addresses: []
     }),
     methods: {
-      onInputChanged(e) {
-        this.searchAdress(e);
-      },
-      searchAdress(addr) {
-        console.log('[Search]', addr);
+      onInputBlured(e) {
+        const query = e.target.value;
+        search('http://localhost:9200/swtest/_search',  query).then(data => {
+          this.addresses = data;
+        }).catch(e => {
+          console.log('error', e);
+        });
       },
       onValueSelected(v) {
         console.log('[Selected]', v);
